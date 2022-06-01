5b6956d051c2520c4922757ec721c20a344e9421
janisollo <sollo.jani@gmail.com>
2022-06-01 12:19:54 +0700

    Selected seats into array

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-let ticketPrice = movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 // Update total and count
 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // Copy selected seats into array
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  console.log(seatsIndex);
+
+  // Map through array
+
+  // Return new array indexes
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
